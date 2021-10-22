eb77007bc14db74668697e208525e0da1ff34186
Vincent Le <49280437+VincentLeV@users.noreply.github.com>
2021-10-22 10:05:21 +0300

    Add baseUrl

--- cypress/integration/app.js
+++ cypress/integration/app.js
@@ -4,7 +4,7 @@ describe('Pokedex', function() {
   // })
 
   it('front page can be opened', function() {
-    cy.visit( 'http://localhost:5000' )
+    cy.visit( Cypress.config().baseUrl )
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
