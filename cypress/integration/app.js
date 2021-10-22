0cc92ff68ad6514d554486c76e1e198cf3c307bd
Vincent Le <49280437+VincentLeV@users.noreply.github.com>
2021-10-22 09:58:31 +0300

    Add config to workflow

--- cypress/integration/app.js
+++ cypress/integration/app.js
@@ -1,9 +1,10 @@
 describe('Pokedex', function() {
-  beforeEach( function() {
-    cy.visit( 'http://localhost:5000' )
-  })
+  // beforeEach( function() {
+  //   cy.visit( 'http://localhost:5000' )
+  // })
 
   it('front page can be opened', function() {
+    cy.visit( 'http://localhost:5000' )
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
